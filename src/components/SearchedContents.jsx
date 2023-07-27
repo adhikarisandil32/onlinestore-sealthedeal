3176e93b0a5b9199f4131113b88fc31ba232dbf3
Sandil Adhikari <adhikarisandil32@gmail.com>
2023-07-28 02:17:08 +0545

    search from text

--- src/components/SearchedContents.jsx
+++ src/components/SearchedContents.jsx
@@ -3,19 +3,30 @@ import { useSearchParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import EachProductCard from './EachProductCard'
 import { getSearchedProducts } from '../store/searchSlice'
+import { updateSearchedProducts } from '../store/searchSlice'
+import { StatusCode } from '../utils/StateStatusThunk'
 
 export default function SearchedContents() {
 
   const dispatch = useDispatch()
   const [searchParams] = useSearchParams()
   
-  const {products: searchedProducts} = useSelector(state => state.searchedProducts)
+  const {searchedProducts, status} = useSelector(state => state.searchedProducts)
 
   useEffect(() => {
     dispatch(
       getSearchedProducts(searchParams.get("category") ? searchParams.get("category") : 'all')
     )
+    dispatch(
+      updateSearchedProducts(searchParams.get("s"))
+    )
   }, [searchParams])
+
+  if(status === StatusCode.LOADING){
+    return(
+      <p className="min-h-[250px] py-4 text-center font-semibold text-xl">Loading...</p>
+    )
+  }
   
   return (
     <div className="px-8 py-4 flex gap-4 flex-wrap justify-center">
--- src/store/searchSlice.jsx
+++ src/store/searchSlice.jsx
@@ -1,31 +1,48 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+import { StatusCode } from "../utils/StateStatusThunk";
 
 const searchSlice = createSlice({
   name: 'search',
   initialState: {
     products: [],
-    status: 'idle'
+    searchedProducts: [],
+    status: StatusCode.IDLE,
+    searchText: ''
   },
   reducers: {
-
+    updateSearchedProducts: (state, action) => {
+      state.searchText = action.payload
+      
+      if(state.searchText){
+        state.searchedProducts = state.products.filter(product => {
+          return (
+            product?.title.includes(state.searchText)
+          )
+        })
+      } else {
+        state.searchedProducts = state.products
+      }
+    },
   },
   extraReducers: (builder) => {
     builder
       .addCase(getSearchedProducts.fulfilled, (state, action) => {
         state.products = action.payload
-        state.status = 'idle'
+        state.searchedProducts = state.products
+        state.status = StatusCode.IDLE
       })
       .addCase(getSearchedProducts.pending, (state, action) => {
-        state.status = 'pending'
+        state.status = StatusCode.LOADING
       })
       .addCase(getSearchedProducts.rejected, (state, action) => {
-        state.status = 'rejected'
+        state.status = StatusCode.ERROR
       })
   }
 })
 
 export const searchReducer = searchSlice.reducer
+export const {updateSearchedProducts} = searchSlice.actions
 
 export const getSearchedProducts = createAsyncThunk("get/searchedProducts", async (category) => {
   const getURL = category.toLowerCase() === 'all' ?
